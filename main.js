bf038007656c429304eb28bddd203e235d4c68a2
AaryaRG <74090082+AaryaRG@users.noreply.github.com>
2021-10-14 17:15:29 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,55 +1,14 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality:90
+});
+camera=document.getElementById("camera");
+Webcam.attach('#camera');
 
-var last_position_of_x, last_position_of_y;
-
-    canvas = document.getElementById('myCanvas');
-    ctx = canvas.getContext("2d");
-    
-    color = "black";
-    width_of_line = 2;
-var width=screen.width;
-new_width=screen.width-70;
-new_height=screen.height-300;
-if (width<992){
-    document.getElementById("myCanvas").width=new_width;
-    document.getElementById("myCanvas").height=new_height;
+function take_snap(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>';
+    });
 }
-    canvas.addEventListener("touchstart", my_touchstart);
-    
-    function my_touchstart(e)
-    {
-        //Addictonal Activity start
-        color = document.getElementById("color").value;
-        width_of_line = document.getElementById("width_of_line").value;
-        //Addictonal Activity ends
-
-        last_position_of_x=e.touches[0].clientX-canvas.offsetLeft;
-        last_position_of_y=e.touches[0].clientY-canvas.offsetTop;
-    }
-
-    
-    canvas.addEventListener("touchmove", my_touchmove);
-    function my_touchmove(e)
-    {
-
-         current_position_of_touch_x = e.touches[0].clientX - canvas.offsetLeft;
-         current_position_of_touch_y = e.touches[0].clientY - canvas.offsetTop;
-
-        
-        ctx.beginPath();
-        ctx.strokeStyle = color;
-        ctx.lineWidth = width_of_line;
-
-        console.log("Last position of x and y coordinates = ");
-        console.log("x = " + last_position_of_x + "y = " + last_position_of_y);
-        ctx.moveTo(last_position_of_x, last_position_of_y);
-
-        console.log("Current position of x and y coordinates = ");
-        console.log("x  = " + current_position_of_touch_x + "y = " + current_position_of_touch_y);
-        ctx.lineTo(current_position_of_touch_x, current_position_of_touch_y);
-        ctx.stroke();
-        
-
-        last_position_of_x = current_position_of_touch_x; 
-        last_position_of_y = current_position_of_touch_y;
-    }
-
